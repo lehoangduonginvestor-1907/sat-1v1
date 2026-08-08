7550824c18504cd1a4c1dcba190894b8b67ef978
lehoangduonginvestor-1907 <lehoangduonginvestor@gmail.com>
2026-08-08 20:40:09 +0700

    fix: serve explanations static directory in backend

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,7 @@ const io = new Server(server, {
 
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'data/images')));
+app.use('/explanations', express.static(path.join(__dirname, 'data/explanations')));
 
 // Lưu trữ các phòng đang active
 const rooms = {};
